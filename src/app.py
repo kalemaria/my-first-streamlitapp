ece647bf975746bc165e93ab254803bd43477098
Ria Salfer <maria.salfer@gmx.de>
2023-12-04 23:22:18 +0100

    Add energy data and a map with number of sources

--- src/app.py
+++ src/app.py
@@ -9,111 +9,79 @@ import json
 from copy import deepcopy
 #st.balloons()
 
-# First some MPG Data Exploration
+# First some basic data exploration
 @st.cache_data
-def load_data(path):
+def load_csv(path):
     df = pd.read_csv(path)
     return df
 
+@st.cache_data
+def load_json(path):
+    with open(path) as response:
+        json_object = json.load(response)
+        return json_object
 
-mpg_df_raw = load_data(path="./data/mpg.csv")
-mpg_df = deepcopy(mpg_df_raw)
+# Data from: https://data.open-power-system-data.org/renewable_power_plants/
+df_raw = load_csv(path="./data/raw/renewable_power_plants_CH.csv")
+df = deepcopy(df_raw)
+
+# Coordinates of the Cantons from: https://data.opendatasoft.com/explore/dataset/georef-switzerland-kanton%40public/export/?disjunctive.kan_code&disjunctive.kan_name&sort=year&location=8,46.82242,8.22403&basemap=jawg.streets&dataChart=eyJxdWVyaWVzIjpbeyJjb25maWciOnsiZGF0YXNldCI6Imdlb3JlZi1zd2l0emVybGFuZC1rYW50b25AcHVibGljIiwib3B0aW9ucyI6eyJkaXNqdW5jdGl2ZS5rYW5fY29kZSI6dHJ1ZSwiZGlzanVuY3RpdmUua2FuX25hbWUiOnRydWUsInNvcnQiOiJ5ZWFyIn19LCJjaGFydHMiOlt7ImFsaWduTW9udGgiOnRydWUsInR5cGUiOiJsaW5lIiwiZnVuYyI6IkNPVU5UIiwic2NpZW50aWZpY0Rpc3BsYXkiOnRydWUsImNvbG9yIjoiIzE0MkU3QiJ9XSwieEF4aXMiOiJ5ZWFyIiwibWF4cG9pbnRzIjoiIiwidGltZXNjYWxlIjoieWVhciIsInNvcnQiOiIifV0sImRpc3BsYXlMZWdlbmQiOnRydWUsImFsaWduTW9udGgiOnRydWV9
+cantons_raw = load_json("./data/raw/georef-switzerland-kanton.geojson")
+cantons = deepcopy(cantons_raw)
+
+# Match the canton code from the df with the canton name in the json
+cantons_dict = {'TG':'Thurgau', 'GR':'Graubünden', 'LU':'Luzern', 'BE':'Bern', 'VS':'Valais', 
+                'BL':'Basel-Landschaft', 'SO':'Solothurn', 'VD':'Vaud', 'SH':'Schaffhausen', 'ZH':'Zürich', 
+                'AG':'Aargau', 'UR':'Uri', 'NE':'Neuchâtel', 'TI':'Ticino', 'SG':'St. Gallen', 'GE':'Genève', 
+                'GL':'Glarus', 'JU':'Jura', 'ZG':'Zug', 'OW':'Obwalden', 'FR':'Fribourg', 'SZ':'Schwyz', 
+                'AR':'Appenzell Ausserrhoden', 'AI':'Appenzell Innerrhoden', 'NW':'Nidwalden', 'BS':'Basel-Stadt'}
+df["canton_name"] = df["canton"].map(cantons_dict)
+
+# Find number of sources per canton
+sources_per_canton = df.groupby("canton_name").size().reset_index(name="count")
 
 # Add title and header
-st.title("Introduction to Streamlit")
-st.header("MPG Data Exploration")
+st.title("My First Dashboard with Streamlit")
+st.header("Renewable Power Plants in Switzerland Data Exploration")
 
 # Widgets: checkbox (you can replace st.xx with st.sidebar.xx)
 if st.checkbox("Show Dataframe"):
     st.subheader("This is my dataset:")
-    st.dataframe(data=mpg_df)
-    # st.table(data=mpg_df)
+    st.dataframe(data=df)
 
-# Setting up columns
-left_column, middle_column, right_column = st.columns([3, 1, 1])
-
-# Widgets: selectbox
-years = ["All"]+sorted(pd.unique(mpg_df['year']))
-year = left_column.selectbox("Choose a Year", years)
-
-# Widgets: radio buttons
-show_means = middle_column.radio(
-    label='Show Class Means', options=['Yes', 'No'])
-
-plot_types = ["Matplotlib", "Plotly"]
-plot_type = right_column.radio("Choose Plot Type", plot_types)
-
-# Flow control and plotting
-if year == "All":
-    reduced_df = mpg_df
-else:
-    reduced_df = mpg_df[mpg_df["year"] == year]
-
-means = reduced_df.groupby('class').mean(numeric_only=True)
-
-# In Matplotlib
-m_fig, ax = plt.subplots(figsize=(10, 8))
-ax.scatter(reduced_df['displ'], reduced_df['hwy'], alpha=0.7)
-
-if show_means == "Yes":
-    ax.scatter(means['displ'], means['hwy'], alpha=0.7, color="red")
-
-ax.set_title("Engine Size vs. Highway Fuel Mileage")
-ax.set_xlabel('Displacement (Liters)')
-ax.set_ylabel('MPG')
-
-# In Plotly
-p_fig = px.scatter(reduced_df, x='displ', y='hwy', opacity=0.5,
-                   range_x=[1, 8], range_y=[10, 50],
-                   width=750, height=600,
-                   labels={"displ": "Displacement (Liters)",
-                           "hwy": "MPG"},
-                   title="Engine Size vs. Highway Fuel Mileage")
-p_fig.update_layout(title_font_size=22)
-
-if show_means == "Yes":
-    p_fig.add_trace(go.Scatter(x=means['displ'], y=means['hwy'],
-                               mode="markers"))
-    p_fig.update_layout(showlegend=False)
-
-# Select which plot to show
-if plot_type == "Matplotlib":
-    st.pyplot(m_fig)
-else:
-    st.plotly_chart(p_fig)
-
-# We can write stuff
-url = "https://archive.ics.uci.edu/ml/datasets/auto+mpg"
-st.write("Data Source:", url)
-# "This works too:", url
-
-# Another header
+# Add a map plot
 st.header("Maps")
 
-# Sample Streamlit Map
-st.subheader("Streamlit Map")
-ds_geo = px.data.carshare()
-ds_geo['lat'] = ds_geo['centroid_lat']
-ds_geo['lon'] = ds_geo['centroid_lon']
-st.map(ds_geo)
-
-# Sample Choropleth mapbox using Plotly GO
+# Choropleth mapbox using Plotly GO
 st.subheader("Plotly Map")
 
-with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
-    counties = json.load(response)
-df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
-                 dtype={"fips": str})
-
-plotly_map = go.Figure(go.Choroplethmapbox(geojson=counties,
-                                           locations=df.fips,
-                                           z=df.unemp,
-                                           colorscale="Viridis",
-                                           zmin=0, zmax=12,
-                                           marker={"opacity": 0.5, "line_width": 0}))
-plotly_map.update_layout(mapbox_style="carto-positron",
-                         mapbox_zoom=3,
-                         mapbox_center={"lat": 37.0902, "lon": -95.7129},
-                         margin={"r": 0, "t": 0, "l": 0, "b": 0})
-
+plotly_map = px.choropleth_mapbox(
+    sources_per_canton, 
+    color="count",
+    geojson=cantons, 
+    locations="canton_name", 
+    featureidkey="properties.kan_name",
+    center={"lat": 46.8, "lon": 8.3},
+    mapbox_style="open-street-map", 
+    zoom=6.3,
+    opacity=0.8,
+    width=900,
+    height=500,
+    labels={"canton_name":"Canton",
+           "count":"Number of Sources"},
+    title="<b>Number of Clean Energy Sources per Canton</b>",
+    color_continuous_scale="Viridis",
+)
+plotly_map.update_layout(
+    margin={"r":0,"t":35,"l":0,"b":0},
+    font={"family":"Sans",
+          "color":"maroon"},
+    hoverlabel={"bgcolor":"white",
+                "font_color":"black", 
+                "font_size":12,
+                "font_family":"Sans"},
+    title={"font_size":20,
+           "xanchor":"left", "x":0.01,
+           "yanchor":"bottom", "y":0.95}
+)
 st.plotly_chart(plotly_map)
